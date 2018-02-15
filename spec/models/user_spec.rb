f825b33d13f455d181ea6e6a7522700bf226c091
Titch Deane <titch@hotmail.co.uk>
2018-02-14 17:16:41 -0800

    Include signup with email confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create!(email: "theotheo@meowmeowmeowm.com", password: 'meowmeow') }
+
+  describe "attributes" do
+    it "should have email and password attributes" do
+      expect(user).to have_attributes(email: user.email, password: user.password)
+    end
+  end
+
+
 end
